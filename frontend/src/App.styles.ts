fd143121c0c0af860265af6618909d2f27bc4c1c
Krzysztof Bujalski <1krzysztof.bujalski@gmail.com>
2022-12-09 13:38:35 +0100

    [FE]mainPage: add global styles for the app

---
+++ frontend/src/App.styles.ts
@@ -0,0 +1,7 @@
+import { styled, Typography } from '@mui/material';
+// import { prepareFontProps, mainTheme } from '@boilerplate-mono/boilerplate-frontend-components';
+
+export const AppWrapper = styled('div')({
+    background: 'linear-gradient(180deg, #0D1633 0%, #14285A 27.6%, #6081D3 100%)',
+    minHeight: '100vh'
+})
