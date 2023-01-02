db9aee98353ad1a5aa9aef7ac1c906ee0c8d2470
Wiktoria Grymuza <w.grymuza@doittechnology.pl>
2023-01-02 21:20:30 +0100

    [BE] add getAll(posts) and getById(post)

--- backend/src/main/java/pl/spot/us/backend/controller/PostController.java
+++ backend/src/main/java/pl/spot/us/backend/controller/PostController.java
@@ -4,15 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.spot.us.backend.entity.Post;
 import pl.spot.us.backend.repository.PostRepository;
+
+import java.util.List;
+
 @RequestMapping("/posts")
 @RestController
 public class PostController {
     @Autowired
     PostRepository postRepository;
     @PostMapping("/")
-    public int add (@RequestBody Post post){
+    public int add(@RequestBody Post post){
         return postRepository.save(post);
     }
+
+    @GetMapping("")
+    public List<Post> getAll(){
+        return postRepository.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable("id") int id){
+        return postRepository.getById(id);
+    }
+
     @GetMapping("/hello")
     public String helloworld(){
         return "witaj świecie";
--- backend/src/main/java/pl/spot/us/backend/repository/PostRepository.java
+++ backend/src/main/java/pl/spot/us/backend/repository/PostRepository.java
@@ -2,6 +2,7 @@ package pl.spot.us.backend.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import pl.spot.us.backend.entity.Post;
@@ -15,5 +16,14 @@ public class PostRepository {
     public int save(Post post){
        jdbcTemplate.update("INSERT INTO Post(tag,content,title) VALUES(?,?)",post.getTag(),post.getContent(),post.getTitle());
         return 1;
+    }
+
+    public List<Post> getAll() {
+        List<Post> posts = jdbcTemplate.query("SELECT * FROM Post", BeanPropertyRowMapper.newInstance(Post.class));
+        return posts;
+    }
+    public Post getById ( int id){
+        return jdbcTemplate.queryForObject("SELECT * FROM Post WHERE" + "id = ?",
+                    BeanPropertyRowMapper.newInstance(Post.class), id);
     }
 }
