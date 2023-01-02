2106ba4048367301d8d66ad7264816a89125e703
B3ziks <beziksor123@gmail.com>
2023-01-02 20:47:29 +0100

    [BE] repo test2

--- backend/src/main/java/pl/spot/us/backend/controller/PostController.java
+++ backend/src/main/java/pl/spot/us/backend/controller/PostController.java
@@ -1,8 +1,20 @@
 package pl.spot.us.backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.spot.us.backend.entity.Post;
+import pl.spot.us.backend.repository.PostRepository;
 @RequestMapping("/posts")
+@RestController
 public class PostController {
-
+    @Autowired
+    PostRepository postRepository;
+    @PostMapping("/")
+    public int add (@RequestBody Post post){
+        return postRepository.save(post);
+    }
+    @GetMapping("/hello")
+    public String helloworld(){
+        return "witaj świecie";
+    }
 }
--- backend/src/main/java/pl/spot/us/backend/repository/PostRepository.java
+++ backend/src/main/java/pl/spot/us/backend/repository/PostRepository.java
@@ -3,16 +3,17 @@ package pl.spot.us.backend.repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 import pl.spot.us.backend.entity.Post;
 
 import java.util.List;
-
+@Repository
 public class PostRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     public int save(Post post){
-//        jdbcTemplate.update("INSERT INTO ");
+       jdbcTemplate.update("INSERT INTO Post(tag,content,title) VALUES(?,?)",post.getTag(),post.getContent(),post.getTitle());
         return 1;
     }
 }
